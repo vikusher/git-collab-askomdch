85b5d344222aeda1a8954e2273678db4e9446dab
vikusher <vikusher@gmail.com>
2024-04-08 19:43:45 -0400

    added config reader

---
+++ src/main/java/com/askomdch/features/config/ConfigReader.java
@@ -0,0 +1,28 @@
+package com.askomdch.features.config;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class ConfigReader {
+
+        private static Properties properties;
+        private static final String PATH = "src/main/resources/config.properties";
+        private ConfigReader(){}
+
+        static {
+            try {
+                FileInputStream inputStream = new FileInputStream(PATH);
+                properties = new Properties();
+                properties.load(inputStream);
+                inputStream.close();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+
+        public static String getProperty(String keyName){
+            return properties.getProperty(keyName);
+        }
+    }
+
+

b7237d166c4e86dc17c7f31c763a208d2627085a
vikusher <vikusher@gmail.com>
2024-04-21 12:15:29 -0400

    added verify shipping

--- src/main/java/com/askomdch/driver/Driver.java
+++ src/main/java/com/askomdch/driver/Driver.java
@@ -1,8 +1,10 @@
 package com.askomdch.driver;
 
 import com.askomdch.config.ConfigReader;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
@@ -20,7 +22,16 @@ public class Driver {
         if (driver == null){
             switch (browser){
                 case "chrome":
-                    driver = new ChromeDriver();
+                    WebDriverManager.chromedriver().setup();
+                    ChromeOptions options = new ChromeOptions();
+//                    options.addArguments("--headless"); // Run in headless mode
+                    options.addArguments("--no-sandbox"); // Bypass OS security model
+//                    options.addArguments("--disable-gpu"); // Applicable to windows os only
+                    options.addArguments("start-maximized"); // Maximize the browser on start
+                    options.addArguments("enable-automation");
+                    options.addArguments("--disable-infobars");
+                    options.addArguments("--disable-dev-shm-usage");
+                    driver = new ChromeDriver(options);
                     break;
                 case "firefox":
                     driver = new FirefoxDriver();
